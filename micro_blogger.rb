58f614e5a7e3f117277d474317223475c59f0d59
Justin Hiltz <justin4770@gmail.com>
2015-04-20 21:04:32 -0400

    Posting tweets now functions

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -7,4 +7,23 @@ class	MicroBlogger
     puts "Initializing MicroBlogger"
     @client = JumpstartAuth.twitter
   end
+
+  def tweet(message)
+  	@client.update(message)
+  end
+
+  def tweet_length(message)
+  	if message.length > 140
+  		puts "Your tweet will only contain the first 140 characters."
+  		message = "".ljust(140, message)
+  		tweet(message)
+  	else
+  		tweet(message)
+  	end
+  end
 end
+
+blogger = MicroBlogger.new
+blogger.tweet_length("This one is another test Tweet.")
+blogger.tweet_length("This is me testing that this message which contains more than 140 characters will not, indeed, post to the Twitter account. What will happen?")
+# Error message from the Twitter API
